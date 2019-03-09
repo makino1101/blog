cd1646ee0318496b3a0719df339a3f39532f8834
makino1101 <makinoshuuhei@mbp.local>
2019-03-09 15:31:50 +0900

    Create content

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -4,6 +4,23 @@ class ContentController < ApplicationController
     @content=Content.all
   end
 
+  def new
+  end
 
+  def show
+  end
+
+  def create
+    Content.create(blog_params)
+  end
+
+  def destroy
+
+  end
+
+  private
+  def blog_params
+    params.permit(:content)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'blog' => 'content#index'
+  get 'blog'          => 'content#index'
+  get 'blog/new'      => 'content#new'
+  post 'blog'         => 'content#create'
 end
